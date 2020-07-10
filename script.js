6e2942ef78b4cdedfed793c50ac9bfc9850d1007
zyxkim <zyxkim.bmstu@gmail.com>
2020-07-10 21:52:48 +0300

    #9 canvas initialization and constant width/height

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 'use strict'
 
 let curColor = [0, 0, 0];
+
+let canvas = document.getElementById('mainCanvas');
+let context = canvas.getContext('2d');
+
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;

db412acfae74c6c8f4764398f811617e64fea581
alina-torr <toropchina_a1@mail.ru>
2020-07-10 21:52:49 +0300

    #4 add basic pencil

---
+++ pencil.js
@@ -0,0 +1,48 @@
+let pencilButton = document.getElementById("pencil");
+
+function drawPencil() { 
+	canvas.addEventListener("mousedown", drawWithPencil);
+}
+
+function drawWithPencil(eventClick) {
+  let startX = eventClick.offsetX;
+  let startY = eventClick.offsetY;
+  
+  context.lineWidth = curToolSize;
+  context.strokeStyle = `rgb(curColor[0],curColor[1],curColor[2])`;
+  context.lineCap = "round";
+  
+  drawPointWithPencil(startX, startY);
+  
+  canvas.addEventListener("mousemove", drawLinesWithPencil);
+	canvas.addEventListener("mouseup", pencilUp);
+	canvas.addEventListener("mouseout", pencilOut);
+}
+
+function pencilUp(eventUp) {
+	context.lineTo(eventUp.offsetX, eventUp.offsetY);
+	context.stroke();
+	canvas.removeEventListener("mousemove", drawLinesWithPencil);
+}
+
+function pencilOut(eventOut) {
+	canvas.removeEventListener("mousemove", drawLinesWithPencil);
+}
+
+function drawLinesWithPencil(eventMove) {
+  context.lineTo(eventMove.offsetX, eventMove.offsetY);
+  context.stroke();
+  context.moveTo(eventMove.offsetX, eventMove.offsetY);
+}
+
+function drawPointWithPencil(startX,startY) {
+  context.moveTo(startX,startY);
+  context.lineTo(startX,startY);
+  context.stroke();
+}
+
+function deletePencil() {
+	canvas.removeEventListener("mousedown", drawWithPencil);
+	canvas.removeEventListener("mouseout", pencilOut);
+	canvas.removeEventListener("mouseup", pencilUp);
+}
--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 'use strict'
 
+let canvas = document.getElementById("mainCanvas");
+let context = canvas.getContext("2d");
+
 let curColor = [0, 0, 0];
+let curToolSize = 5;

dab2978f3942a091c6ba4808de9f6021c332ed8c
zyxkim <zyxkim.bmstu@gmail.com>
2020-07-11 16:04:02 +0300

    #27 fixed brush functions structure

--- js/brushes.js
+++ js/brushes.js
@@ -1,39 +1,41 @@
 'use strict'
 
+let isDrawing = false;
+
 function initBasicBrush() {
-  let isDrawing = false;
-
-  function startPoint(e) {
-    isDrawing = true;
-    drawBasicBrush(e);
-  }
-
-  function endPoint() {
-    isDrawing = false;
-    context.beginPath();
-  }
-
-  function drawBasicBrush(e) {
-    if (!isDrawing) return;
-
-    context.lineWidth = curToolSize;
-    context.lineJoin = 'round';
-    context.lineCap = 'round';
-    context.strokeStyle = 'rgb(' + curColor[0] + ',' + curColor[1] + ',' + curColor[2] + ')';
-
-    context.lineTo(e.offsetX, e.offsetY);
-    context.stroke();
-    context.beginPath();
-    context.moveTo(e.offsetX, e.offsetY);
-  }
-
   canvas.addEventListener("mousedown", startPoint);
-  canvas.addEventListener("mousemove", drawBasicBrush);
-  canvas.addEventListener("mouseup", endPoint);
 }
 
 function deleteBasicBrush() {
   canvas.removeEventListener("mousedown", startPoint);
-  canvas.removeEventListener("mousemove", drawBasicBrush);
+  canvas.removeEventListener("mousemove", drawLine);
   canvas.removeEventListener("mouseup", endPoint);
+}
+
+function startPoint(e) {
+  isDrawing = true;
+
+  context.lineWidth = curToolSize;
+  context.lineJoin = 'round';
+  context.lineCap = 'round';
+  context.strokeStyle = 'rgb(' + curColor[0] + ',' + curColor[1] + ',' + curColor[2] + ')';
+
+  drawLine(e);
+
+  canvas.addEventListener("mousemove", drawLine);
+  canvas.addEventListener("mouseup", endPoint);
+}
+
+function endPoint() {
+  isDrawing = false;
+  context.beginPath();
+}
+
+function drawLine(e) {
+  if (!isDrawing) return;
+
+  context.lineTo(e.offsetX, e.offsetY);
+  context.stroke();
+  context.beginPath();
+  context.moveTo(e.offsetX, e.offsetY);
 }

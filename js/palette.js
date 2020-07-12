f457eb3e637787a5e9431275bc72d6c9f8ebbdef
Julia Belikova <yulyawh@gmail.com>
2020-07-12 11:04:29 +0300

    Обработка ошибок

--- js/palette.js
+++ js/palette.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 let colorInput = document.querySelector('#color');
 
@@ -7,12 +7,15 @@ colorInput.addEventListener('input', () => {
 });
 
 function hexToRgb(hex) {
-  if (hex === '')
-    return;
-  if (hex.length < 7) {
-    hex = '#' + hex[1] + hex[1] + hex[2] + hex[2] + hex[3] + hex[3];
-  }
-  return [parseInt(hex.substr(1, 2), 16),
+  if ((hex.length === 4 || hex.length === 7) && hex[0] === '#') {
+    if (hex.length < 7) {
+      hex = '#' + hex[1] + hex[1] + hex[2] + hex[2] + hex[3] + hex[3];
+    }
+    let rgb = [parseInt(hex.substr(1, 2), 16),
           parseInt(hex.substr(3, 2), 16),
           parseInt(hex.substr(5, 2), 16)];
+    if (rgb[0] < 256 && rgb[1] < 256 && rgb[2] < 256)
+      return rgb;
+  }
+  throw "Wrong color code";
 }

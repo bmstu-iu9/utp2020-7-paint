e5cdf5af9bb81b0f326fabdcfd35ba644bddf045
Julia Belikova <yulyawh@gmail.com>
2020-07-12 12:16:39 +0300

    Обработка ошибок

--- js/palette.js
+++ js/palette.js
@@ -9,7 +9,7 @@ colorInput.addEventListener('input', () => {
 function hexToRgb(hex) {
   let r1 = hex.match(/^#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/i);
   let r2 = hex.match(/^#([0-9a-f])([0-9a-f])([0-9a-f])$/i);
-  if ((hex.length === 7 || r1) || (hex.length === 4 && r2)) {
+  if (r1 || r2) {
     if (hex.length < 7) {
       hex = '#' + hex[1] + hex[1] + hex[2] + hex[2] + hex[3] + hex[3];
     }

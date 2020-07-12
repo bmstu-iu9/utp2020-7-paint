364f0d2266f19abc9efdc9c62ac9cff9e782a8ed
Julia Belikova <yulyawh@gmail.com>
2020-07-12 12:08:10 +0300

    Обработка ошибок

--- js/palette.js
+++ js/palette.js
@@ -7,15 +7,15 @@ colorInput.addEventListener('input', () => {
 });
 
 function hexToRgb(hex) {
-  if ((hex.length === 4 || hex.length === 7) && hex[0] === '#') {
+  let r1 = hex.match(/^#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/i);
+  let r2 = hex.match(/^#([0-9a-f])([0-9a-f])([0-9a-f])$/i);
+  if ((hex.length === 7 || r1) || (hex.length === 4 && r2)) {
     if (hex.length < 7) {
       hex = '#' + hex[1] + hex[1] + hex[2] + hex[2] + hex[3] + hex[3];
     }
-    let rgb = [parseInt(hex.substr(1, 2), 16),
+    return [parseInt(hex.substr(1, 2), 16),
           parseInt(hex.substr(3, 2), 16),
           parseInt(hex.substr(5, 2), 16)];
-    if (rgb[0] < 256 && rgb[1] < 256 && rgb[2] < 256)
-      return rgb;
   }
   throw "Wrong color code";
 }

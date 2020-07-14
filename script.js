89837e51e812808fa05bceb8a75e573968191fb0
Julia Belikova <yulyawh@gmail.com>
2020-07-14 11:04:03 +0300

    Изменение горячих клавиш

--- script.js
+++ script.js
@@ -87,28 +87,18 @@ document.getElementById("clear").addEventListener('click', () => {
   context.clearRect(0, 0, canvas.width, canvas.height);
 });
 
-document.addEventListener('keydown', (event) => {
-  switch (event.keyCode) {
-  case 67:
+addEventListener('keydown', (event) => {
+  if (event.altKey && event.which == 67) {
     document.getElementById("clear").click();
-    console.log("Cleared");
-    break;
-  case 80:
+  } else if (event.altKey && event.which == 80) {
     colorInput.focus();
     colorInput.click();
-    console.log("The palette is open");
-    break;
-  case 83:
+  } else if (event.altKey && event.which == 83) {
     downloadBtn.click();
-    console.log("Saved");
-    break;
-  case 85:
+  } else if (event.altKey && event.which == 85) {
     document.getElementById("uploadImage").click();
-    console.log("Choose file");
-    break;
-  default:
-    console.log("Wrong key");
-}});
+  }
+});
 
 changeCanvasHeight.oninput = function () {
   let height = document.getElementById("changeCanvasHeight").value;

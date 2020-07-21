0eb5d8f5863932ecd6a06896c7038585802bddbb
Julia Belikova <yulyawh@gmail.com>
2020-07-21 05:03:01 +0300

    changed the state of the eyedropper inside and outside the canvas

--- js/eyedropper.js
+++ js/eyedropper.js
@@ -6,21 +6,28 @@ let eyedropperWindow = document.getElementById('eyedropperWindow');
 function initEyedropper() {
   canvas.addEventListener('mousemove', handleEyedropper);
   canvas.addEventListener('click', stopEyedropper);
+  canvas.addEventListener('mouseenter', switchEyedropperWindow);
+  canvas.addEventListener('mouseout', switchEyedropperWindow);
 }
 
 function deleteEyedropper() {
   canvas.removeEventListener('mousemove', handleEyedropper);
   canvas.removeEventListener('click', stopEyedropper);
+  canvas.removeEventListener('mouseenter', switchEyedropperWindow);
+  canvas.removeEventListener('mouseout', switchEyedropperWindow);
 }
 
 function stopEyedropper(event) {
-
   let eventLocation = getEventLocation(this, event);
   let color = getPixelColor(eventLocation.x, eventLocation.y);
   curColor = color;
   colorInput.value = "#" + rgbToHex(color);
-  eyedropperWindow.hidden = true;
-  deleteEyedropper();
+  switchEyedropperWindow();
+  eyedropperButton.dispatchEvent(new Event('click'));
+}
+
+function switchEyedropperWindow() {
+  eyedropperWindow.hidden = !eyedropperWindow.hidden;
 }
 
 function getPixelColor(x, y) {
@@ -29,15 +36,13 @@ function getPixelColor(x, y) {
 }
 
 function handleEyedropper(event) {
-  eyedropperWindow.hidden = false;
-
   let eventLocation = getEventLocation(this, event);
   let color = getPixelColor(eventLocation.x, eventLocation.y);
   eyedropperWindow.style.background = arrayToRgb(color);
 
   function moveWindow(x, y) {
-    eyedropperWindow.style.left = (x + 10) + 'px';
-    eyedropperWindow.style.top = (y + 40) + 'px';
+    eyedropperWindow.style.left = x + 10 + 'px';
+    eyedropperWindow.style.top = y + 40 + 'px';
   }
 
   moveWindow(eventLocation.x, eventLocation.y);

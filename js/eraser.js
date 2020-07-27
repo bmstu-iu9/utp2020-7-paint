3193b0de92ab806b68b04e4ecc7321027491e30e
alina-torr <toropchina_a1@mail.ru>
2020-07-27 17:48:11 +0300

    #95 change algorithm of eraser

--- js/eraser.js
+++ js/eraser.js
@@ -3,12 +3,11 @@
 let eraserParameters = {
   oldX: 0,
   oldY: 0,
-  newX: 0,
-  newY: 0,
-  distance: 0,
+  newX: 0, 
+  newY: 0, 
+  distance: 0, 
   angle: 0,
-  imageData: context.getImageData(0, 0, canvas.width, canvas.height),
-}
+};
 
 function initEraser() {
   canvas.addEventListener("mousedown", startPointEraser);
@@ -19,18 +18,29 @@ function deleteEraser() {
   canvas.removeEventListener("mousemove", drawEraser);
   canvas.removeEventListener("mouseup", endPoint);
   canvas.removeEventListener("mouseleave", endPoint);
+  context.globalCompositeOperation = "source-over";
 }
 
 function startPointEraser(e) {
   isDrawing = true;
   if (!isReplaying) rememberDrawingTool("Eraser");
   
-  eraserParameters.imageData = context.getImageData(0, 0, canvas.width, canvas.height);
   eraserParameters.oldX = e.offsetX;
   eraserParameters.oldY = e.offsetY;
-  drawPointEraser(e.offsetX, e.offsetY);
-  context.putImageData(eraserParameters.imageData, 0, 0);
 
+  context.globalCompositeOperation = "destination-out";
+  context.lineWidth = 0.1;
+  context.fillStyle = arrayToRgb(curColor);
+  context.strokeStyle = arrayToRgb(curColor);
+
+  context.beginPath();
+  context.arc(eraserParameters.oldX, eraserParameters.oldY, curToolSize / 2, 0, Math.PI * 2, false);
+  context.fill();
+  context.stroke();
+  context.closePath();
+
+  drawEraser(e);
+  
   canvas.addEventListener("mousemove", drawEraser);
   canvas.addEventListener("mouseup", endPoint);
   canvas.addEventListener("mouseleave", endPoint);
@@ -40,53 +50,18 @@ function drawEraser(e) {
   if (!isDrawing) return;
   if (!isReplaying) curCords[curState - 1].cords.push([e.offsetX, e.offsetY]);
 
-  eraserParameters.imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-  eraserParameters.newX = e.offsetX;
-  eraserParameters.newY = e.offsetY; 
-  
-  eraserParameters.distance = Math.sqrt((e.offsetX - eraserParameters.oldX) ** 2 + (e.offsetY - eraserParameters.oldY) ** 2);
+  eraserParameters.distance = Math.sqrt(Math.pow(e.offsetX - eraserParameters.oldX, 2) + Math.pow(e.offsetY - eraserParameters.oldY, 2));
   eraserParameters.angle = Math.atan2(e.offsetX - eraserParameters.oldX, e.offsetY - eraserParameters.oldY);
-  
+
   for (let i = 0; i < eraserParameters.distance; i++) {
-    eraserParameters.newX = Math.floor(eraserParameters.oldX + i * Math.sin(eraserParameters.angle));
-    eraserParameters.newY = Math.floor(eraserParameters.oldY + i * Math.cos(eraserParameters.angle));
-    eraserParameters.deltaX = eraserParameters.newX - eraserParameters.oldX;
-    eraserParameters.deltaY = eraserParameters.newY - eraserParameters.oldX;
-    
-    drawPointEraser(eraserParameters.newX, eraserParameters.newY);
+    eraserParameters.newX = eraserParameters.oldX + i * Math.sin(eraserParameters.angle);
+    eraserParameters.newY = eraserParameters.oldY + i * Math.cos(eraserParameters.angle);
+    context.beginPath();
+    context.arc(eraserParameters.newX, eraserParameters.newY, curToolSize / 2, 0, Math.PI * 2, false);
+    context.fill();
+    context.stroke();
   }
-  
-  context.putImageData(eraserParameters.imageData, 0, 0);
+
   eraserParameters.oldX = e.offsetX;
   eraserParameters.oldY = e.offsetY;
-}
-
-function drawPointEraser(x, y) {
-  let radius = Math.floor(curToolSize / 2);
-  let x0 = 0;
-  let y0 = radius;
-  let delta = 1 - 2 * radius;
-  let error = 0;
-  while (y0 >= 0) {
-    for (let i = x - x0; i <= x + x0; i++) if (areInCanvas(i, y - y0)) changePixel(i, y - y0);
-    for (let i = x - x0; i <= x + x0; i++) if (areInCanvas(i, y + y0)) changePixel(i, y + y0);
-   
-    error = 2 * (delta + y0) - 1;
-    if ((delta < 0) && (error <= 0)) {
-      delta += 2 * (++x0) + 1;
-      continue;
-    }
-    if ((delta > 0) && (error > 0)) {
-      delta -= 1 + 2 * (--y0);
-      continue;
-    }
-    delta += 2 * (++x0 - --y0);
-  }
-  
-  function changePixel(x, y) {
-    eraserParameters.imageData.data[getIndexOfRedInData(x, y)] = 0;
-    eraserParameters.imageData.data[getIndexOfGreenInData(x, y)] = 0;
-    eraserParameters.imageData.data[getIndexOfBlueInData(x, y)] = 0;
-    eraserParameters.imageData.data[getIndexOfAlphaInData(x, y)] = 0;
-  }
 }

12cc1fb0df812c82e3727f5076f9497872fe999b
Janb0t <dyachekp@gmail.com>
2020-07-28 20:10:24 +0300

    #118 updated Layer class

--- js/layers.js
+++ js/layers.js
@@ -29,6 +29,7 @@ function createLayerHtml(id) {
   lockBtn.title = 'Заблокировать'
   newLayer.appendChild(lockBtn);
 
+  console.log(newLayer);
   return newLayer;
 }
 
@@ -45,20 +46,40 @@ function parseLayerId(str) {
     case 'lay':
       return parseInt(str.slice('layerDisplay'.length));
       break;
+    case 'loc':
+      return parseInt(str.slice('lockLayer'.length));
+      break;
+  }
+  return null;
+}
+
+function parseLayerBtnId(str) {
+  switch (str.slice(0, 3)) {
+    case 'hid':
+      return parseInt(str.slice('hideLayer'.length));
+      break;
     case 'pre':
       return parseInt(str.slice('preview'.length));
       break;
   }
+  return null;
 }
 
 function getLayerByDisplay(target) {
   let id = parseLayerId(target);
-  return layers[id];
+  if (id) return layers[id];
+  return null;
+}
+
+function getLayerByBtn(target) {
+  let id = parseLayerBtnId(target);
+  if (id) return layers[id];
+  return null;
 }
 
 function switchLayer(event) {
   let layer = getLayerByDisplay(event.target.id);
-  if (activeLayer.id === layer.id) return;
+  if (!layer || activeLayer.id === layer.id) return;
   activeLayer.canvas.style.pointerEvents = "none";
   layer.canvas.style.pointerEvents = "auto";
   activeLayer.display.classList.remove('highlight');
@@ -76,11 +97,20 @@ class Layer {
       this.id = 0;
       this.display = document.getElementById('layerDisplay0');
       this.display.addEventListener('click', switchLayer);
+      this.preview = document.getElementById('preview0');
+
       this.canvas = document.getElementById('layer0');
-      this.preview = this.display.children[0];
-      this.index = 50;
       this.canvas.style.zIndex = this.index;
       this.canvas.style.background = 'repeat url(\"img/background.png\")';
+
+      this.hide = document.getElementById('hideLayer0');
+      this.hide.addEventListener('click', hideLayerHandler);
+      this.hidden = false;
+      this.lock = document.getElementById('lockLayer0');
+      this.lock.addEventListener('click', lockLayerHandler);
+      this.locked = false;
+
+      this.index = 50;
       layers.push(this);
       return this;
     }
@@ -105,8 +135,14 @@ class Layer {
     activeInstrument && activeInstrument.init();
     activeLayer = this;
 
-
-    this.preview = this.display.children[0];
+    this.preview = document.getElementById('preview' + this.id);
+    this.hide = document.getElementById('hideLayer' + this.id);
+    console.log('hideLayer' + this.id);
+    this.hide.addEventListener('click', hideLayerHandler);
+    this.hidden = false;
+    this.lock = document.getElementById('lockLayer' + this.id);
+    this.lock.addEventListener('click', lockLayerHandler);
+    this.locked = false;
 
     if (caller === 'addLayerTop') {
       topLayer.display.before(this.display);
@@ -144,4 +180,23 @@ function changePreview() {
   let previewContext = activeLayer.preview.getContext('2d');
   previewContext.clearRect(0, 0, activeLayer.preview.width, activeLayer.preview.height);
   previewContext.drawImage(canvas, 0, 0, activeLayer.preview.width, activeLayer.preview.height);
+}
+
+function hideLayerHandler(event) {
+  let layer = getLayerByBtn(event.target.id);
+  if (!layer) return;
+  if (layer.hidden) {
+    layer.hidden = false;
+    layer.canvas.style.visibility = 'visible';
+    layer.hideBtn.title = 'Скрыть';
+  } else {
+    layer.hidden = true;
+    layer.canvas.style.visibility = 'hidden';
+    layer.hideBtn.title = 'Показать';
+  }
+}
+
+function lockLayerHandler(event) {
+  let layer = getLayerByBtn(event.target.id);
+  if (!layer) return;
 }

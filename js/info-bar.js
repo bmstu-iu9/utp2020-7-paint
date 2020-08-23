b9111bcd705a840e0008bedcb9abfd00a2642d7e
Timur-Ionov <t-ionov@list.ru>
2020-08-23 13:32:19 +0300

    added getCoords on canvas

---
+++ js/info-bar.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function getCurCoordsOnCanvas(event) {
+  let eventLocation = getEventLocation(canvas, event);
+
+  return {
+    x :  eventLocation.x - curCanvasBorder,
+    y :  eventLocation.y - curCanvasBorder
+  };
+}
+
+function showCurCoordsOnCanvas() {
+
+  function printCurCoords(event) {
+    //display somewhere on info bar after design
+    //and remove listener when bar is closed
+    let coords = getCurCoordsOnCanvas(event);
+
+
+  }
+
+  canvas.addEventListener('mousemove', printCurCoords);
+}
